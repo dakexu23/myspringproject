f0091e65d1115ede2536e8e36752c9c51dc12cd6
大可 <dakexu23@gmail.com>
2022-03-10 19:45:19 +0800

    first commit

---
+++ src/main/java/com/ainvestornotes/lab/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.ainvestornotes.lab;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @GetMapping("/hello")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
